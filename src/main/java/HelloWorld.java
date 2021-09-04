ef95545cc5fce91082cf660f38d63780bf5bead6
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-09-04 10:26:25 +0800

    Save signup list
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.IOException;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        Document document = Jsoup.connect("https://ithelp.ithome.com.tw/2021ironman/signup/list").get();
+        FileUtils.write(new File("list.html"), document.toString(), "utf-8");
+    }
+}
