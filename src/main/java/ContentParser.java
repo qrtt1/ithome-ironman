f82792f8d6124ba169dab9aedbff262fd6996ca4
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-09-04 22:04:52 +0800

    Extract topics from a page
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- src/main/java/ContentParser.java
+++ src/main/java/ContentParser.java
@@ -1,5 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,6 +8,8 @@ import java.io.IOException;
 public class ContentParser {
     public static void main(String[] args) throws IOException {
         Document document = Jsoup.parse(new File("list.html"), "utf-8");
-        System.out.println(document);
+        for (Element element : document.select("div.border-frame.clearfix > div.contestants-wrapper > div.contestants-list")) {
+            System.out.println(element.text());
+        }
     }
 }
