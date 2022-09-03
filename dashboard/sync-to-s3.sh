a0d991ef0ccf00bb23ae39779f86efafe88af68a
Ching Yi, Chan <qrtt1@infuseai.io>
2022-09-03 15:03:29 +0800

    update s3 syncer
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- dashboard/sync-to-s3.sh
+++ dashboard/sync-to-s3.sh
@@ -1,2 +1,6 @@
 yarn build
+<<<<<<< Updated upstream
 aws s3 sync build/ s3://ithome-2021-ironman --exclude "*.json" --acl public-read --delete
+=======
+aws s3 sync build/ s3://ithome-ironman-watcher/2022/ --exclude "*.json" --acl public-read --delete
+>>>>>>> Stashed changes
