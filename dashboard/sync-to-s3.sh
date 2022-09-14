186680d62c2d04456a70aabbbe102963e8e63795
Ching Yi, Chan <qrtt1@infuseai.io>
2022-09-14 19:35:49 +0800

    Fix sync script
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- dashboard/sync-to-s3.sh
+++ dashboard/sync-to-s3.sh
@@ -2,4 +2,4 @@ yarn build:prod
 aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2022/ --acl public-read --delete --exclude "data.json"
 
 yarn build:prod_v2
-aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2022v2/ --acl public-read --delete --exclude "data.json"
+aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2022v2/ --acl public-read --delete --exclude "ui-data.json"
