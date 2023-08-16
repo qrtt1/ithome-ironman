5276783ebfd0c48847856ecefe45cf77af330829
Ching Yi, Chan <qrtt1@infuseai.io>
2023-08-16 18:28:29 +0800

    update frontend
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- dashboard/sync-to-s3.sh
+++ dashboard/sync-to-s3.sh
@@ -1,5 +1,2 @@
 yarn build:prod
-aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2022/ --acl public-read --delete --exclude "data.json"
-
-yarn build:prod_v2
-aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2022v2/ --acl public-read --delete --exclude "ui-data.json" --exclude "data.json"
+aws s3 --profile alan sync build/ s3://ithome-ironman-watcher/2023/ --acl public-read --delete --exclude "ui-data.json" --exclude "data.json"
